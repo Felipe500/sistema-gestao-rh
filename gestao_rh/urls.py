6e3acb2ed6a41ef3456c6bac88b1ebf95af5dcb5
jose felipe <felipe.brx.dev@gmail.com>
2022-02-04 14:15:29 -0300

    add requirements.txt

--- gestao_rh/urls.py
+++ gestao_rh/urls.py
@@ -1,21 +1,18 @@
-"""gestao_rh URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+
+    path('funcionarios/', include('apps_rh.funcionario.urls')),
+    path('', include('apps_rh.core.urls')),
+
+    path('departamentos/', include('apps_rh.departamento.urls')),
+    path('empresa/', include('apps_rh.empresa.urls')),
+    path('documento/', include('apps_rh.documentos.urls')),
+    path('horas-extras/', include('apps_rh.registro_hora_extra.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
